38ea58f068378cb64febeaca4aba9d30abb6ae7c
jpassehl <JACQUELINEPASSEHL@gmail.com>
2025-09-15 22:18:07 -0500

    setting up responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Grid } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container>
+        <Grid bgcolor={"coral"} size={12}>
+          Header, size=12
+        </Grid>
+        <Grid bgcolor={"yellow"} size={6}>
+          Sidebar, size=6
+        </Grid>
+        <Grid bgcolor={"blue"} size={6}>
+          Main Content, size=6
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
-export default App
+export default App;
